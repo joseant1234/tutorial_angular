97f05a903441741e34aef72a6488bb3d8a5c14b9
Jose Antonio <j-osea-nt@hotmail.com>
2018-06-12 11:30:20 -0500

    set up component

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCardComponent implements OnInit {
 
+  public name : string;
+  public username : string;
+  public avatar : string
+
   constructor() { }
 
   ngOnInit() {
+    this.name = "Hola";
+    this.username = "jaja";
+    this.avatar = "https://cdn-images-1.medium.com/max/705/1*7GZIq-Pg4VsrgWObJVi0SQ.png"
   }
 
 }
