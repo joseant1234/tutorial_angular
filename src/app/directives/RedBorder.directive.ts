0d693323a7411a64abbbf407e3cd012971af7b74
Jose Antonio <j-osea-nt@hotmail.com>
2018-06-16 10:19:48 -0500

    add hostlistener (events) to attributte directive

--- src/app/directives/RedBorder.directive.ts
+++ src/app/directives/RedBorder.directive.ts
@@ -2,7 +2,9 @@
 // ElementRef es un servicio q se va inyectar para acceder al elemento html nativo de aquel q se esta modificando, para acceder a su estilo, id, ...
 // registrar la directiva en el app.module
 // se usa Input para recibir parametros en la directiva
-import { Directive, ElementRef, Input} from '@angular/core';
+// Agregar eventos al elemento en el q se esta aplicando la directiva de atributo, se puede  via un decorador llamado HostListener
+// HostListener permite decorar funciones para q estas se ejecuten una vez q un evento sucede
+import { Directive, ElementRef, Input, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[redBorder]'
@@ -18,7 +20,18 @@ export class RedBorder{
       console.log('Usando directiva')
       // permite acceder al dom a traves de angular,
       // evitar interactuar de manera directa con el dom, usar las estraegias q angular expone para interactuar con el DOM, como el nativeElement
-      this.el.nativeElement.style.border = `solid 2px ${this.redBorder}`
+      // this.el.nativeElement.style.border = `solid 2px ${this.redBorder}`;
       this.el.nativeElement.classList.add(this.redClass);
     }
+
+    // recibe como argumento el evento q va estar escuchando (click,.. eventos del dom)
+    // aplica listener o escucha de un evento al host o contenedor (es aquel q hace referencia a la directiva personalizada), por tanto se puede aplicar listener eventos al elemento al q se este aplicando la directiva de atributo
+    @HostListener("mouseover") applyBorder(){
+      this.el.nativeElement.style.border = `solid 2px ${this.redBorder}`;
+    }
+
+    @HostListener("mouseout") removeBorder(){
+      this.el.nativeElement.style.border = "none";
+    }
+
 }
